34ff01440bf47672c2e86372c13328e09ac900d1
mkkeems <mkkeems@gmail.com>
2024-12-05 03:08:55 +0900

    setting up google auth init and callback api routes, get google tokens on auth success

--- components/GoogleSignInButton.tsx
+++ components/GoogleSignInButton.tsx
@@ -1,14 +1,22 @@
-import { generateGoogleUrl } from "@/lib/google/generateGoogleUrl";
 import { Button } from "./ui/button";
 import { Icons } from "./ui/icons";
 
 export const GoogleSignInButton = () => {
   const handleGoogleSignIn = async () => {
-    const googleUrlWithToken = await generateGoogleUrl();
-    // const response = await fetch(googleUrlWithToken);
-    // console.log(response, response.json());
-    // const { redirectUrl } = await response.json();
-    window.location.href = googleUrlWithToken;
+    try {
+      const response = await fetch("/api/auth/google/init");
+      const { redirectUrl } = await response.json();
+
+      if (redirectUrl) {
+        console.log("Redirecting to:", redirectUrl);
+        window.location.href = redirectUrl;
+      } else {
+        console.error("Failed to retrieve Google OAuth URL");
+        window.location.href = "/";
+      }
+    } catch (error) {
+      console.error("Error during Google Sign-In initialization:", error);
+    }
   };
 
   return (
@@ -17,7 +25,7 @@ export const GoogleSignInButton = () => {
       className="flex items-center gap-2 px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-lg shadow-md transition duration-200"
     >
       <Icons.google className="w-6 h-6" />
-      <span>Continue with Google</span>
+      Continue with Google
     </Button>
   );
 };
